baaf4b2feb4cfd71068d4ef1a8aa96ebf3a95a08
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-03-30 12:49:20 +0100

    Set up manual seed data

--- bogus-efcore-demo/ProductsDemo/ProductContext.cs
+++ bogus-efcore-demo/ProductsDemo/ProductContext.cs
@@ -12,14 +12,48 @@ public class ProductContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        // Necessary to use a composite key
         modelBuilder.Entity<ProductProductCategory>().HasKey(x => new { x.ProductId, x.CategoryId });
 
-        // Old fashioned way: (TODO SET UP)
-        //modelBuilder.Entity<Product>().HasData(new Product { Id = 1, CreationDate = DateTimeOffset.Now, Name = "Headphones", ProductProductCategories = new ProductCategory[] { new ProductCate () { } } })
+        //SetupManualSeedData(modelBuilder);
+
+        //SetupSeedDataWithBogus(modelBuilder);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private static void SetupManualSeedData(ModelBuilder modelBuilder)
+    {
+        var product1 = new Product
+        {
+            Id = 1,
+            Name = "Laptop",
+            CreationDate = DateTimeOffset.Parse("2024-04-15T18:00:00+01:00")
+        };
+
+        var product2 = new Product
+        {
+            Id = 2,
+            Name = "C# In Depth",
+            CreationDate = DateTimeOffset.Parse("2024-04-15T19:00:00+01:00")
+        };
+
+        var category1 = new ProductCategory() { Id = 1, Name = "Technology" };
+        var category2 = new ProductCategory() { Id = 2, Name = "Books" };
 
 
-        // STEP: Enable this to showcase Bogus
+        var joinedCategory1 = new ProductProductCategory { ProductId = product1.Id, CategoryId = category1.Id };
+        var joinedCategory2 = new ProductProductCategory { ProductId = product2.Id, CategoryId = category1.Id };
+        var joinedCategory3 = new ProductProductCategory { ProductId = product2.Id, CategoryId = category2.Id };
+
+        modelBuilder.Entity<Product>().HasData(product1, product2);
+        modelBuilder.Entity<ProductCategory>().HasData(category1, category2);
+        modelBuilder.Entity<ProductProductCategory>().HasData(joinedCategory1, joinedCategory2, joinedCategory3);
+    }
+
+
+    private static void SetupSeedDataWithBogus(ModelBuilder modelBuilder)
+    {
         //// Generate seed data with Bogus
         //var databaseSeeder = new DatabaseSeeder();
 
@@ -28,7 +62,5 @@ public class ProductContext : DbContext
         //modelBuilder.Entity<ProductCategory>().HasData(databaseSeeder.ProductCategories);
         //modelBuilder.Entity<ProductProductCategory>().HasData(databaseSeeder.ProductProductCategories);
 
-
-        base.OnModelCreating(modelBuilder);
     }
 }
--- bogus-efcore-demo/ProductsDemo/README.md
+++ bogus-efcore-demo/ProductsDemo/README.md
@@ -1,4 +1,9 @@
 # Demo content
 - Run database in docker (`docker compose up -d`)
 - Run `dotnet ef database update` if it doesn't exist yet
+- Showcase entering items into database by hand (Slow, not repeatable)
+- Showcase using HasData manually by uncommenting that code in `ProductContext`
+  - run `dotnet ef migrations add AddSeedDataByHand`
+-
 - Demo what you would normally do to set up testing data
+- 
