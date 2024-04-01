ccfca79a291f83690a9de33b7711217cea93ba5c
Brian Chavez <bchavez@bitarmory.com>
2024-03-31 20:33:30 -0600

    BogusBasics: initial commit.

---
+++ bogus-intro-demo/BogusBasics/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using static System.Console;
+
+namespace BogusBasics;
+
+internal static class ExtensionsForConsole
+{
+   public static void Dump(this User usr)
+   {
+      WriteLine($"  Id: {usr.Id}");
+      WriteLine($"  IsActive: {usr.IsActive}");
+      WriteLine($"  DateCreated: {usr.DateCreated}");
+      WriteLine($"  FirstName: {usr.FirstName}");
+      WriteLine($"  LastName: {usr.LastName}");
+      WriteLine($"  Email: {usr.Email}");
+
+   }
+}
