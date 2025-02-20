a5ca4b2961ad4da3932a20a5acf9c6ecc59e5ef5
Sander ten Brinke <s.tenbrinke2@gmail.com>
2025-02-20 11:58:15 +0100

    Create demo for UseSeeding

---
+++ BogusEfCoreUseSeeding/DatabaseSeeder.cs
@@ -0,0 +1,79 @@
+﻿using Bogus;
+using BogusEfCoreUseSeeding;
+
+namespace BogusEfCoreHasData;
+
+public class DatabaseSeeder
+{
+    public List<Product> Products { get; } = [];
+    public List<ProductCategory> ProductCategories { get; } = [];
+    public List<ProductProductCategory> ProductProductCategories { get; } = [];
+
+    public DatabaseSeeder()
+    {
+        // This project performs data existance checks with the name, so this has a unique index.
+        // Bogus doesn't have a big enough dataset to guarantee unique names for a GIANT data set, so I just limited the numbers here for ease of use.
+        Products = GenerateProducts(amount: 100);
+        ProductCategories = GenerateProductCategories(amount: 22);
+        ProductProductCategories = GenerateProductProductCategories(amount: 200, Products, ProductCategories);
+    }
+
+    private static List<Product> GenerateProducts(int amount)
+    {
+        var productFaker = new Faker<Product>()
+            .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+            // The refDate is very important! Without it, it will generate a random date based on the CURRENT date on your system.
+            // Generating a date based on the system date is not deterministic!
+            // So the solution is to pass in a constant date instead which will be used to generate a random date
+            .RuleFor(x => x.CreationDate, f => f.Date.FutureOffset(
+                refDate: new DateTimeOffset(2024, 4, 15, 18, 0, 0, TimeSpan.FromHours(1))));
+
+        // DEMO: Uncomment this line
+        //.RuleFor(x => x.Description, f => f.Commerce.ProductDescription());
+
+        var products = Enumerable.Range(1, amount)
+            .Select(i => SeedRow(productFaker, i))
+            .ToList();
+
+        return products;
+    }
+
+    private static List<ProductCategory> GenerateProductCategories(int amount)
+    {
+        var categoryFaker = new Faker<ProductCategory>()
+            .RuleFor(x => x.Name, f => f.Commerce.Categories(1).First());
+
+        var categories = Enumerable.Range(1, amount)
+            .Select(i => SeedRow(categoryFaker, i))
+            .ToList();
+
+        return categories;
+    }
+
+    private static List<ProductProductCategory> GenerateProductProductCategories(
+        int amount,
+        IEnumerable<Product> products,
+        IEnumerable<ProductCategory> productCategories)
+    {
+        // Now we set up the faker for our join table.
+        // We do this by grabbing a random product and category that were generated.
+        var productProductCategoryFaker = new Faker<ProductProductCategory>()
+            .RuleFor(x => x.Product, f => f.PickRandom(products))
+            .RuleFor(x => x.Category, f => f.PickRandom(productCategories));
+
+        var productProductCategories = Enumerable.Range(1, amount)
+            .Select(i => SeedRow(productProductCategoryFaker, i))
+            // We do this GroupBy() + Select() to remove the duplicates from the generated join table entities
+            .GroupBy(x => new { ProductName = x.Product.Name, CategoryName = x.Category.Name })
+            .Select(x => x.First())
+            .ToList();
+
+        return productProductCategories;
+    }
+
+    private static T SeedRow<T>(Faker<T> faker, int rowId) where T : class
+    {
+        var recordRow = faker.UseSeed(rowId).Generate();
+        return recordRow;
+    }
+}
--- BogusEfCoreUseSeeding/Worker.cs
+++ BogusEfCoreUseSeeding/Worker.cs
@@ -1,23 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spectre.Console;
+
 namespace BogusEfCoreUseSeeding;
 
-public class Worker : BackgroundService
+public class Worker(IServiceScopeFactory serviceScopeFactory) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+    private readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ProductContext>();
+
+        var products = await dbContext.Products
+            .Include(x => x.ProductProductCategories)
+            .ThenInclude(x => x.Category)
+            .ToListAsync();
+
+        DrawTable(products);
+    }
+
+    private static void DrawTable(List<Product> products)
+    {
+        DrawLine();
+
+        DrawBigText("Demo");
+
+        // Creating the table
+        var table = new Table();
+        table.AddColumn("ID");
+        table.AddColumn("Name");
+        // DEMO: Uncomment this line
+        //table.AddColumn("Description");
+        table.AddColumn("Categories");
+        table.AddColumn("CreationDate");
+
+        // Populating the table with product entries
+        foreach (var product in products.Take(20))
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
+            var getRowsToRender = GetRowsToRender(product);
+            table.AddRow(getRowsToRender);
+        }
+
+        table.Centered();
+
+        // Rendering the table
+        AnsiConsole.Write(table);
+
+        DrawBigText($"Total: {products.Count} products");
+
+        static void DrawLine()
+        {
+            var rule = new Rule("[green]EF <3 Bogus[/]");
+            AnsiConsole.Write(rule);
+        }
+
+        static void DrawBigText(string text)
+        {
+            AnsiConsole.Write(new FigletText(text).Centered());
         }
     }
+
+    private static string[] GetRowsToRender(Product product) => [
+        product.Id.ToString(),
+        product.Name,
+        // DEMO: Uncomment this line
+        //product.Description,
+        string.Join(",", product.ProductProductCategories.Select(x => x.Category.Name)),
+        product.CreationDate.ToString()
+    ];
 }
